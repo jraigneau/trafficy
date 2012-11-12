44d390ef27266823f20220d209fe5b4bc98d35e8
Julien Raigneau <julien.raigneau@free.fr>
2012-11-12 09:59:27 -0500

    added helper for morning/evening logic

--- Gemfile
+++ Gemfile
@@ -1,15 +1,5 @@
 source :gemcutter
 gem 'sinatra', '1.3.3'
 gem 'haml', '3.1.7'
-gem 'data_mapper', '1.2.0'
 gem 'nokogiri', '1.5.5'
 gem 'sequel', '3.41.0'
-
-
-group :production do
-  gem 'dm-postgres-adapter', '1.2.0'
-end
-
-group :development do
-  gem 'dm-sqlite-adapter', '1.2.0'
-end
--- main.rb
+++ main.rb
@@ -16,7 +16,20 @@ require './init'
 helpers do
   def link_to text, url
     "<a href='#{ URI.encode url }'>#{ text }</a>"
-  end 
+  end
+  #vérifie si la date est le matin ou le soir (après / avant 13h) => defaut = matin
+  def is_morning? date
+    if date.nil? or date == 0
+        true
+    else
+        if DateTime.strptime(date, "%Y-%m-%d_%H-%M").hour < 13
+            true
+        else
+            false
+        end
+    end
+            
+  end
 end
 
 get '/' do
@@ -27,27 +40,33 @@ end
 #https://maps.google.fr/maps?saddr=14+Rue+de+Lorraine,+Asni%C3%A8res-sur-Seine&daddr=26+Rue+de+la+Rochefoucauld,+Boulogne-Billancourt
 
 get '/run/:date' do
-  if :date != ""
-    Path.each do |x|
-      doc = Nokogiri::HTML(open("https://maps.google.fr/maps?saddr=#{ x.origin }&daddr=#{ x.destination }"))  
-      data = doc.xpath("//*[@id='altroute_0']/div/div[2]/span")
-      if data.length != 0
-        data = data.text.split(":")[1].split(" ")
-        #first result: "Dans les conditions actuelles de circulation : 1 heure 10 min" 
-      else
-        data = doc.xpath("//*[@id='altroute_0']/div/div[1]/span").text.split("km")[1].split(" ")
-      end
-      min = 0
-      if data.length > 2 #more than 1 hour
-        min = data[0].to_i*60 + data[2].to_i
-      else
-        min = data[0].to_i
-      end
-      puts "nb min:" + min.to_s
+    if :date != ""
+        Path.each do |x|
+            origin = x.origin
+            destination = x.destination
+            if !is_morning(:date) #Si c'est le soir, on inverse destination et origin
+                origin,destination = destination, origin
+            end
+            
+            doc = Nokogiri::HTML(open("https://maps.google.fr/maps?saddr=#{ origin }&daddr=#{ destination }"))  
+            data = doc.xpath("//*[@id='altroute_0']/div/div[2]/span")
+            if data.length != 0 #il y a des bouchons
+                data = data.text.split(":")[1].split(" ")
+                #first result: "Dans les conditions actuelles de circulation : 1 heure 10 min" 
+            else #pas de bouchon => pas le même code html
+                data = doc.xpath("//*[@id='altroute_0']/div/div[1]/span").text.split("km")[1].split(" ")
+            end
+            
+            min = 0
+            if data.length > 2 #more than 1 hour
+                min = data[0].to_i*60 + data[2].to_i
+            else
+                min = data[0].to_i
+            end
+            puts "nb min:" + min.to_s
 
+        end
     end
-    
-      end
 end
 
 
